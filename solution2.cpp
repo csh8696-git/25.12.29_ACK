90aacd3a99e48011fb41c2f162d47aadb5f4e0a1
csh8696-git <csh8696@gmail.com>
2025-12-29 14:06:10 +0900

    solution3_20
    정수 내림차순으로 배치하기

--- solution2.cpp
+++ solution2.cpp
@@ -17,61 +17,61 @@ long long solution(long long n) {
 }
 
 
-#include <string>
-#include <vector>
-#include <math.h>
-using namespace std;
-
-long long solution(long long n) {
-    long long answer = sqrt(n);
-
-    return powl(answer, 2) == n ? powl(answer + 1, 2) : -1;
-}
-
-#include <string>
-#include <vector>
-
-using namespace std;
-
-long long solution(long long n) {
-    long long answer = 0;
-
-    long long i;
-    for (i = 1; i * i <= n; i++);
-    if ((i - 1) * (i - 1) == n)
-    {
-        answer = i * i;
-    }
-
-    else {
-        answer = -1;
-    }
-
-
-
-    return answer;
-}
-
-long long solution(long long n) {
-    long long answer = 0, i = 0;
-    for (i = 1; i * i <= n; i++)
-        if (i * i == n)
-            return (i + 1) * (i + 1);
-    return -1;
-}
-
-#include <string>
-#include <vector>
-#include <algorithm> // sort 사용 위해 필요 
-#include <string>
-#include <numeric>
-
-using namespace std;
-
-long long solution(long long n) {
-    long long answer = 0;
-    if (sqrt(n) == floor(sqrt(n)))
-        return pow(sqrt(n) + 1, 2);
-    else
-        return -1;
-}
+//#include <string>
+//#include <vector>
+//#include <math.h>
+//using namespace std;
+//
+//long long solution(long long n) {
+//    long long answer = sqrt(n);
+//
+//    return powl(answer, 2) == n ? powl(answer + 1, 2) : -1;
+//}
+//
+//#include <string>
+//#include <vector>
+//
+//using namespace std;
+//
+//long long solution(long long n) {
+//    long long answer = 0;
+//
+//    long long i;
+//    for (i = 1; i * i <= n; i++);
+//    if ((i - 1) * (i - 1) == n)
+//    {
+//        answer = i * i;
+//    }
+//
+//    else {
+//        answer = -1;
+//    }
+//
+//
+//
+//    return answer;
+//}
+//
+//long long solution(long long n) {
+//    long long answer = 0, i = 0;
+//    for (i = 1; i * i <= n; i++)
+//        if (i * i == n)
+//            return (i + 1) * (i + 1);
+//    return -1;
+//}
+//
+//#include <string>
+//#include <vector>
+//#include <algorithm> // sort 사용 위해 필요 
+//#include <string>
+//#include <numeric>
+//
+//using namespace std;
+//
+//long long solution(long long n) {
+//    long long answer = 0;
+//    if (sqrt(n) == floor(sqrt(n)))
+//        return pow(sqrt(n) + 1, 2);
+//    else
+//        return -1;
+//}
---
+++ solution3.cpp
@@ -0,0 +1,36 @@
+﻿#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+long long solution(long long n) {
+    long long answer = 0;
+    vector<int> a;
+
+    while (n) {
+        a.push_back(n % 10);
+        n /= 10;
+    }
+    sort(a.begin(), a.end(), greater<int>());
+    for (int i = 0; i < a.size(); i++) {
+        answer *= 10;
+        answer += a[i];
+    }
+    return answer;
+}
+
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <functional>
+using namespace std;
+
+long long solution(long long n) {
+    long long answer = 0;
+
+    string str = to_string(n);
+    sort(str.begin(), str.end(), greater<char>());
+    answer = stoll(str);
+
+    return answer;
+}
